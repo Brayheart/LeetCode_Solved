62db02c72bfe52b9e7a6668ad12c26993b856655
Tyler Bray <tylerhbray@gmail.com>
2024-03-26 12:00:00 -0700

    checktree

--- index.js
+++ index.js
@@ -5,3 +5,9 @@
 var sum = function (num1, num2) {
   return num1 + num2;
 };
+
+// 2236. Root Equals Sum of Children
+
+var checkTree = function (root) {
+  return root.val === root.left.val + root.right.val;
+};
